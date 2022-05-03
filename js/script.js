f06d0af19b8f97511533bf628fad9fbf2659f92e
Gabry17 <gbianchi528@gmail.com>
2022-05-03 16:27:32 +0200

    iniziato a fare il milestone 3

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ const posts = [
             name: "Pinco Pallo",
             image: "https://unsplash.it/300/300?image=15"
         },
-        likes: 0,
+        likes: 90,
         created: "2018-07-20"
     }
 ];
@@ -65,13 +65,13 @@ posts.forEach( (element)=>{
                     </div>
                     <div class="post-meta__data">
                         <div class="post-meta__author">${element.author.name}</div>
-                        <div class="post-meta__time">4 mesi fa</div>
+                        <div class="post-meta__time">${element.created}</div>
                     </div>                    
                 </div>
             </div>
             <div class="post__text">${element.content}</div>
             <div class="post__image">
-                <img src="${element.media}" alt="">
+                <img src="${element.media}" alt="${element.media}">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
@@ -88,6 +88,15 @@ posts.forEach( (element)=>{
             </div>            
         </div>
     `;
+    
+    const a = document.querySelector('a');
+    const b = document.querySelector('b');
+    
+    a.addEventListener('click', function(){
+        this.classList.add('red');
+        let result = element.likes + 1;
+        b.innerHTML = `${result}`;
+    })
 })
 
 //M3
