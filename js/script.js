5c812e6417733d4a8185729b25cfd82beccc8331
Gabry17 <gbianchi528@gmail.com>
2022-05-03 14:58:02 +0200

    scaffolding con layout html stile css

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//M1
+//creare array di oggetti
+//ogni oggetto deve avere proprieta id,nome,foto,data,testo,img,like
+
+//M2
+//stampare ogni proprieta dell oggetto nell html
+
+//M3
+//cliccando tasto mi piace cambiare colore al testo e aggiungere 1 like in piu
+//ogni volta che mettiamo like inseriamo in un nuovo array l id del post
